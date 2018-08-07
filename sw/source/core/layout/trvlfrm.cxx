e733a3693d2311b3fa0750f728a3a0c685d0f0d2
Michael Stahl <Michael.Stahl@cib.de>
2018-08-07 10:33:08 +0200

    sw: avoid asserts from SwSortedObjs::Insert()
    The first frame is almost certainly already inserted in line 2057;
    This causes "<SwSortedObjs::Insert()> - already contains object".
    
    Change-Id: I4a785accdec033865da971bf25d0ebe06ec2e765

--- sw/source/core/layout/trvlfrm.cxx
+++ sw/source/core/layout/trvlfrm.cxx
@@ -2492,7 +2492,10 @@ void SwRootFrame::CalcFrameRects(SwShellCursor &rCursor)
             if ( pContent->IsInFly() )
             {
                 const SwAnchoredObject* pObj = pContent->FindFlyFrame();
-                aSortObjs.Insert( *const_cast<SwAnchoredObject*>(pObj) );
+                if (!aSortObjs.Contains(*pObj))
+                {   // is this even possible, assuming valid cursor pos.?
+                    aSortObjs.Insert( *const_cast<SwAnchoredObject*>(pObj) );
+                }
             }
 
             // Consider only frames which have the same IsInDocBody value like pStartFrame
